224a4899424abc87b84c5efb440c5ad025a8084a
Taylor <taylorfulner@gmail.com>
2021-06-21 20:29:44 -0400

    added create restaurant controller and view

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -10,10 +10,32 @@ namespace RestaurantRater.Controllers
     public class RestaurantController : Controller
     {
         private RestaurantDbContext _db = new RestaurantDbContext();
+
         // GET: Restaurant
         public ActionResult Index()
         {
             return View(_db.Restaurants.ToList());
+        }
+
+        //GET: Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
         }
     }
 }
